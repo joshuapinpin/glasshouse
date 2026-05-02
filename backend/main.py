9052f235e185fc93c354cf64ff471b6d4d977433
joshuapinpin <joshuapinpin421@gmail.com>
2026-05-02 17:53:50 +1200

    routers

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
-from backend.app.routers.auth import router as auth_router
+from app.routers.auth import router as auth_router
+from app.routers.transactions import router as transactions_router
+from app.routers.fundraisers import router as fundraisers_router
 
 app = FastAPI()
 app.include_router(auth_router)
+app.include_router(transactions_router)
+app.include_router(fundraisers_router)
 
 @app.get("/")
 async def root():
